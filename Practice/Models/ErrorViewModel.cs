f895eece75b52b632f90eaa8cc4bf6d8ff4abffe
Saransh.Chaturvedi <Saransh.Chaturvedi@FAPL609>
2022-05-26 15:16:03 +0530

    Add project files.

---
+++ Practice/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Practice.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
