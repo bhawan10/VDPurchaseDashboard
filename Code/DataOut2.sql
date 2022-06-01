f3d7c0ade509f1a0bef080c6e5b6704ea9856c68
Saransh S Chaturvedi <53694991+Saransh03@users.noreply.github.com>
2022-06-01 17:27:41 +0530

    Add files via upload
    Updated queries

---
+++ Code/DataOut2.sql
@@ -0,0 +1,33 @@
+CREATE PROCEDURE getVendors @Expeditor nvarchar(50)
+AS
+select Location from [dbo].[VendorExpeditorMapping]
+where Expeditor=@Expeditor;
+GO
+
+
+CREATE PROCEDURE getPurchaseOrder @Location nvarchar(200)
+AS
+select * from [FalconTest].[Master].[PurchaseOrder]
+where Location=@Location and Status='Accepted by vendor';
+GO
+
+CREATE PROCEDURE getDistinctCategoryIds @POId nvarchar(30)
+AS
+select distinct [FalconTest].[dbo].[Item].RawMaterialCategoryId
+from [FalconTest].[dbo].[Item]
+inner join [FalconTest].[Relate].[PurchaseOrderItemMapping] on [FalconTest].[Relate].[PurchaseOrderItemMapping].ItemId = [FalconTest].[dbo].[Item].ITEM_ID
+where [FalconTest].[Relate].[PurchaseOrderItemMapping].POId = @POId
+GO
+
+
+CREATE PROCEDURE getItemsFromOperation @Operation nvarchar(30) @POId nvarchar(30)
+AS
+select [dbo].[Item].ITEM_ID,[dbo].[Item].ITEM_DESCRIPTION
+FROM [dbo].[ProcessOperation] 
+INNER JOIN [dbo].[Item] 
+ON [dbo].[ProcessOperation].ProcessId=[dbo].[Item].RawMaterialCategoryId
+INNER JOIN 
+[Relate].[PurchaseOrderItemMapping]
+ON [dbo].[Item].ITEM_ID=[Relate].[PurchaseOrderItemMapping].ItemId
+Where [dbo].[ProcessOperation].OperationId =@Operation and [Relate].[PurchaseOrderItemMapping].PoId = @POId
+GO
