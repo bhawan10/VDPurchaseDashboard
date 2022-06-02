133f8b0694014d6a73f5eb0649722094068841be
bhawan10 <106309012+bhawan10@users.noreply.github.com>
2022-06-02 17:33:47 +0530

    Add files via upload
    Backend to Database Connection

---
+++ Code/Controllers/CRUDController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestForm.Models;
+using System.Data;
+
+namespace TestForm.Controllers
+{
+    public class CRUDController : Controller
+    {
+        public IActionResult Vendor()
+        {
+            CRUDModel model = new CRUDModel();
+            string name = "Ankur.AD375";
+            DataTable dt = model.GetAllVendors(name);
+            
+            return View("Vendor",dt);
+        }
+        public IActionResult PurchaseOrder()
+        {
+            CRUDModel model = new CRUDModel();
+            string vendorSelected = "Toshi Automatic Systems Pvt. Ltd";
+            DataTable dt = model.GetAllVendors(vendorSelected);
+            Console.WriteLine(dt);
+            return View("PurchaseOrder", dt);
+        }
+        public IActionResult ItemsFromOperation()
+        {
+            CRUDModel model = new CRUDModel();
+            string OperationId = "10";
+            string POId = "859";
+            DataTable dt = model.GetItemsFromOperation(OperationId, POId);
+            return View("PurchaseOrder", dt);
+        }
+        public IActionResult Category()
+        {
+            CRUDModel model = new CRUDModel();
+            string POId = "859";
+            DataTable dt = model.getDistinctCategoryIds(POId);
+            return View("POId", dt);
+        }
+    }
+}
