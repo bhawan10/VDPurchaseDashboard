c495a910334866ff6b6fcb290826ce77aaf4e401
Saransh S Chaturvedi <53694991+Saransh03@users.noreply.github.com>
2022-06-13 17:55:43 +0530

    Add files via upload
    Cascaded Dropdowns

--- Code/Controllers/CRUDController.cs
+++ Code/Controllers/CRUDController.cs
@@ -15,20 +15,45 @@ namespace TestForm.Controllers
             this.repo = _repository;
         }
 
-        public IActionResult Vendor(string expeditorName)
+        public IActionResult Vendor()
         {
-            
-            return View("Vendor",repo.getVendors(expeditorName));
+            return View("Vendor");
         }
-        public IActionResult PurchaseOrder(string vendorName)
+        public List<string> GetAllVendors()
+        {
+            string expeditorName = "Ankur.AD375";
+            DataTable check = repo.getVendors(expeditorName); 
+            List<string> result = new List<string>();
+            foreach(DataRow row in check.Rows)
+            {
+                result.Add(row["Location"].ToString());
+            }
+            return result;
+        }
+        /*public IActionResult PurchaseOrder(string vendorName)
         {
             return View("PurchaseOrder", repo.getPOs(vendorName));
+        }*/
+
+        public List<string> PurchaseOrder(string vendor)
+        {
+            //string vendorName2 = "Micromatic Manufacturing systems Pvt Ltd";
+            //Console.WriteLine("hello");
+            //Console.WriteLine(vendor);
+            DataTable check = repo.getPOs(vendor);
+            List<string> POs = new List<string>();
+            foreach (DataRow row in check.Rows)
+            {
+                POs.Add(row["PONumber"].ToString());
+            }
+            return POs;
         }
+
         public IActionResult ItemsFromOperation(string OperationId, string POId)
         {
             return View("Operation", repo.getItemsfromOperation(OperationId, POId));
         }
-        public IActionResult Category()
+        public IActionResult Category(IFormCollection frm)
         {
             string POId = "859";
             return View("RawMaterial", repo.getDistinctCategory(POId));
