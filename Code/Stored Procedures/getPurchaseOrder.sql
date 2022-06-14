e9058f4006a314d71ef330ea6589a02c99978918
navam19314 <73428458+navam19314@users.noreply.github.com>
2022-06-14 10:51:30 +0530

    Delete Code/Stored Procedures directory

--- Code/Stored Procedures/getPurchaseOrder.sql
+++
@@ -1,9 +0,0 @@
-CREATE PROCEDURE getPurchaseOrder @Location nvarchar(200)
-AS
-select PONumber from [Master].[PurchaseOrder] as tbl_PO with (NOLOCK)
-where tbl_PO.SupplierId=
-(select ID from [FalconTest].[dbo].[Vendor_Master_Job_work] with (NOLOCK)
-where Vendor_Name=@Location) 
- and Status='Accepted by vendor';
-GO
-
--- Code/Stored Procedures/getVendors.sql
+++
@@ -1,5 +0,0 @@
-CREATE PROCEDURE getVendors @Expeditor nvarchar(200)
-AS
-select Location from [dbo].[VendorExpeditor] with (NOLOCK)
-where Expeditor=@Expeditor;
-GO
