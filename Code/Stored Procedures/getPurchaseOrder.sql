6332e5075f9b0e74ad3ec983513e1cdf1465538f
navam19314 <73428458+navam19314@users.noreply.github.com>
2022-07-04 17:49:01 +0530

    Delete Code/Stored Procedures directory

--- Code/Stored Procedures/getPurchaseOrder.sql
+++
@@ -1,5 +0,0 @@
-CREATE PROCEDURE getPurchaseOrder @Id bigint
-AS
-Select PkId, PONumber from [Master].[PurchaseOrder] as tbl_PO with (NOLOCK)
-where tbl_PO.SupplierId=@Id  and Status='Accepted by vendor';
-GO
