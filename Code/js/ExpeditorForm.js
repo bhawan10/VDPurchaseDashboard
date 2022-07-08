a43dbbb6c50433e41410fb197142a8abfc23d973
bhawan10 <106309012+bhawan10@users.noreply.github.com>
2022-07-08 12:49:41 +0530

    Add files via upload

--- Code/js/ExpeditorForm.js
+++ Code/js/ExpeditorForm.js
@@ -6,14 +6,24 @@ $(function (expeditorGlobal) {
         expeditorGlobal.mainData = $('#mainData');
         expeditorGlobal.vendor = $('#vendor');
         expeditorGlobal.PO = $('#PO');
+        expeditorGlobal.submitButton = $('#finalSubmit');
         expeditorGlobal.operation = $('#operation');
-        expeditorGlobal.items = $('form');
+        expeditorGlobal.items = $('#form');
+        expeditorGlobal.dict = {};
+        expeditorGlobal.kendoNotification = $('#notification').kendoNotification({
+            position: {
+                top: 60
+            },
+            stacking: 'down',
+            autoHideAfter: 1500
+        });
         initialiseDropDowns();
     };
     var initialiseDropDowns = function () {
-        //console.log(expeditorGlobal.mainData.data());
+        expeditorGlobal.submitButton.on('click', onButtonPress);
+
         expeditorGlobal.vendor.kendoDropDownList({
-            placeholder: 'Please Select Vendor',
+            optionLabel: 'Please Select Vendor',
             height: 300,
             dataValueField: 'id',
             dataTextField: 'text',
@@ -28,7 +38,7 @@ $(function (expeditorGlobal) {
         });
 
         expeditorGlobal.PO.kendoDropDownList({
-            placeholder: 'Please Select PO',
+            optionLabel: 'Please Select PO',
             height: 300,
             
             dataValueField: 'id',
@@ -46,7 +56,7 @@ $(function (expeditorGlobal) {
 
 
         expeditorGlobal.operation.kendoDropDownList({
-            placeholder: 'Please Select Operation',
+            optionLabel: 'Please Select Operation',
             height: 300,
 
             dataValueField: 'id',
@@ -71,30 +81,70 @@ $(function (expeditorGlobal) {
                 }
             },
             autoBind: false,
-            columns: [{
-                field: "itemDescription",
-                title: "Item Description"
+            editable: "incell",
+            width: "70%",
+            columns: [
+            {
+                title: "TSPR Number / Model Number",
+                template: '<div><div class="text-right"><strong>#=toolNo#-#=station#-#=positionNo#-#=reworkNo#</strong></div><div class="text-right">#=modelNo#</div></div>',
+                editable: function () { return false; }
             },
             {
-                field: "toolNo",
-                title: "Tool Number"
-            },
-            {
-                field: "station",
-                title: "Station"
-            },
-            {
-                field: "positionNo",
-                title: "Position Number"
-            },
-            {
-                field: "reworkNo",
-                title: "Rework Number"
+                field: "doneQuantity",
+                title: "Quantity",
+                width: "30%",
+                template: '<div><input style="max-width: 60%; text-align:center;" class="kendo-editable-cell" value="#=doneQuantity#"></input><span> / #= givenQuantity# </span></div>',
+                editor: function (container, options) {
+                    var check = options.model.givenQuantity;
+                    console.log(check);
+                    $('<div><input  style="max-width: 60%; text-align:center;" type="number" data-bind="value:doneQuantity" class="k-textbox" required /><span> /' + check + '</span></div>').appendTo(container);
+                }
+            }],
+            cellClose: function (event) {
+                if (validateGrid(event.model.doneQuantity, event.model.givenQuantity)) {
+                    var entry = {};
+
+                    var today = new Date();
+                    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+                    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+                    var dateTime = date + ' ' + time;
+
+                    entry.POItemID = event.model.id;
+                    entry.OperationId = expeditorGlobal.OperationID;
+                    entry.totalQuantity = event.model.givenQuantity;
+                    entry.doneQuantity = event.model.doneQuantity;
+                    entry.entryBy = "Ankur.AD375";
+                    entry.POId = expeditorGlobal.POID;
+                    entry.isActive = true;
+                    entry.isCompleted = false;
+                    entry.entryDate = dateTime;
+                    expeditorGlobal.dict[entry.POItemID] = entry;
+                }
             }
-            ]
         })
         
     };
+
+    var validateGrid = function (done, given) {
+        var isValid = true;
+        if (done > given) {
+            showErrorNotification("Quantity can not be more than total");
+            isValid = false;
+        }
+        return isValid;
+    };
+
+    var showSuccessNotification = function (message) {
+        expeditorGlobal.kendoNotification.data('kendoNotification').success(message);
+    };
+
+    var showErrorNotification = function (message) {
+        if (!message) {
+            message = 'Some error occured, please contact IT team';
+        }
+        expeditorGlobal.kendoNotification.data('kendoNotification').error(message);
+    };
+
     var onVendorSelect = function (event) {
 
         var oldUrl = '', newUrl = '';
@@ -147,6 +197,7 @@ $(function (expeditorGlobal) {
         var oldUrl = '', newUrl = '';
         var dataItem = event.sender.dataItem();
         var itemSelect = expeditorGlobal.items.data('kendoGrid');
+        /*console.log(itemSelect.dataSource.transport.options.read.url);*/
         oldUrl = itemSelect.dataSource.transport.options.read.url;
         if (dataItem) {
             expeditorGlobal.OperationID = dataItem.id.toString();
@@ -158,7 +209,41 @@ $(function (expeditorGlobal) {
         itemSelect.dataSource.transport.options.read.url = newUrl;
         itemSelect.dataSource.read();
         itemSelect.dataSource.transport.options.read.url = oldUrl;
-        console.log(itemSelect.dataSource.transport.options.read.url);
+    };
+
+
+    var onButtonPress = function () {
+        var checkList = [];
+        for (var key in expeditorGlobal.dict) {
+            checkList.push(expeditorGlobal.dict[key]);
+        }
+        if (checkList.length > 0) {
+            $.ajax({
+                url: expeditorGlobal.mainData.data('check'),
+                type: 'POST',
+                data: {
+                    forms: checkList,
+                },
+                success: function () {
+                    console.log("done");
+                    showSuccessNotification("Form submitted!");
+                    //dict clear
+                    //for (var del in expeditorGlobal.dict) {
+                    //    if (expeditorGlobal.dict.hasOwnProperty(del)) {
+                    //        delete expeditorGlobal.dict[del];
+                    //    }
+                    //}
+                },
+                error: function (data) {
+                    debugger;
+                    console.log("error");
+                    console.log(data);
+                }
+            })
+        }
+        else {
+            showErrorNotification("Form not submitted");
+        }
     };
 
     expeditorGlobal.onDocumentReady = function () {
